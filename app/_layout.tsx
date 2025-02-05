d3ceac9291064e72363036bffa393b7a6fc16100
faqqqke <131741212+faqqqke@users.noreply.github.com>
2025-02-05 05:53:15 +0000

    05.02

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,16 +3,21 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
-
+import { StyleSheet,  Text,  TouchableOpacity } from 'react-native';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { View } from 'react-native';
+import styled from 'styled-components';
+import { blue } from 'react-native-reanimated/lib/typescript/Colors';
+import { TextInput } from 'react-native-gesture-handler';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [counter, setCounter] = useState(0)
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -27,13 +32,47 @@ export default function RootLayout() {
     return null;
   }
 
+  const handlePress = () => {
+    setCounter((val) => val + 1)
+  }
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handlePress}>
+          <Text style={styles.button}>click</Text>
+        </TouchableOpacity>
+        <Text style={styles.text}>
+          {counter}
+        </Text>
+        <TextInput style={styles.input}
+        placeholder='введите задачу'
+
+        returnKeyType='done'/>
+      </View>
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop:100,
+    flexDirection: "row"
+  },
+  button: {
+    color: "#000000",
+    backgroundColor: "ffffff",
+    padding: 50
+  },
+  text: {
+    color: "#ff0000",
+    fontSize: 64
+  },
+  input: {
+    width: 250,
+    fontSize: 20,
+    fontWeight: 100,
+  }
+});
+
+
